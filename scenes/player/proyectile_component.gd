e4ed668f32dd22763e804c8584b81ffa1eec39c3
c-dez <c-dez@proton.me>
2026-01-14 12:01:08 -0600

    agregado dano a bala de jugador y aplicado dano a enemy

--- scenes/player/proyectile_component.gd
+++ scenes/player/proyectile_component.gd
@@ -6,6 +6,7 @@ class_name ProyectileComponent
 @onready var bullet: PackedScene = preload('res://assets/bullet/bullet.tscn')
 @onready var camera: Camera3D = get_parent().get_node('PlayerCamera')
 @onready var muzzle: Marker3D = camera.get_node('Muzzle')
+@onready var player: PlayerBody = get_parent()
 
 
 func _ready() -> void:
@@ -15,11 +16,6 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	_set_position()
 
-	# if Input.is_action_just_pressed('left_mb'):
-	# 	var b = bullet.instantiate()
-	# 	muzzle.add_child(b)
-
-	
 
 func _physics_process(_delta: float) -> void:
 	if ray.is_colliding():
@@ -27,8 +23,9 @@ func _physics_process(_delta: float) -> void:
 			var target = ray.get_collision_point()
 			var b = bullet.instantiate()
 			b.type = b.PLAYER
+			b.damage = player.stats.damage
 			muzzle.add_child(b)
-			b.look_at(target,Vector3.UP)
+			b.look_at(target, Vector3.UP)
 
 	
 	pass
