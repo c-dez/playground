d84d98c54660a6343e4b46aaa795f9ffe50055c1
c-dez <c-dez@proton.me>
2026-01-13 21:00:18 -0600

    creado idle y chase states

--- scenes/enemies/base_enemy/chase.gd
+++ scenes/enemies/base_enemy/chase.gd
@@ -4,25 +4,25 @@ class_name Chase
 var parent: Enemy
 @onready var player: PlayerBody = get_tree().get_first_node_in_group('player')
 
-var check_state_time: float = 3.0
+var check_state_time: float
+var _check_state_time: float = 3.0
 
 func enter() -> void:
 	if parent == null:
 		parent = get_parent().parent
-	check_state_time = 3.0
+	check_state_time = _check_state_time
 
 
 func process(_delta: float) -> void:
-	# check_state_time -= _delta
-	# if check_state_time < 0:
-	# 	_change_state_to_idle()
-	_change_state_to_idle(_delta)
-	pass
+	# cada 3 segundos checa si:
+	check_state_time -= _delta
+	if check_state_time < 0:
+		_change_state_to()
+		check_state_time = _check_state_time
 	
-
+	
 func physics_process(_delta: float) -> void:
 	parent.navigation.target_position = Vector3(player.global_position.x, parent.global_position.y, player.global_position.z)
-	# parent.navigation.target_position = player.global_position
 
 	var current_pos := parent.global_position
 	var next_pos := parent.navigation.get_next_path_position()
@@ -30,18 +30,16 @@ func physics_process(_delta: float) -> void:
 
 	parent.velocity = direction * parent.stats.move_speed
 
-	parent.look_at(parent.navigation.target_position)
+	parent.get_node('MeshInstance3D').look_at(parent.navigation.target_position)
 
 
 func exit() -> void:
-	check_state_time = 3.0
+	check_state_time = _check_state_time
 
 
-func _change_state_to_idle(_delta) -> void:
-	if parent.global_position.distance_to(player.global_position) > parent.stats.attack_range:
-		# check_state_time -= _delta
-		# print(check_state_time)
-		# if check_state_time < 0 :
-		# 	emit_signal('change_state_to', self, 'idle')
-
+func _change_state_to() -> void:
+	# idle
+	if parent.global_position.distance_to(player.global_position) > parent.stats.chase_range:
 		emit_signal('change_state_to', self, 'idle')
+	# attack
+	
--- scenes/enemies/base_enemy/idle.gd
+++ scenes/enemies/base_enemy/idle.gd
@@ -2,10 +2,10 @@ extends State
 class_name Idle
 
 
-var parent
+var parent: Enemy
 @onready var player: PlayerBody = get_tree().get_first_node_in_group('player')
-var check_state_time:float
-var _check_state_time :float = 1.0
+var check_state_time: float
+var _check_state_time: float = 1.0
 
 func enter() -> void:
 	if parent == null:
@@ -14,10 +14,10 @@ func enter() -> void:
 
 	pass
 func process(_delta: float) -> void:
-	# check_state_time -= _delta
-	# if check_state_time < 0:
-	# 	_change_state_to_chase()
-	_change_state_to_chase()
+	check_state_time -= _delta
+	if check_state_time < 0:
+		_change_state_to()
+		check_state_time = _check_state_time
 
 
 func physics_process(_delta: float) -> void:
@@ -27,7 +27,7 @@ func exit() -> void:
 	check_state_time = _check_state_time
 
 
-func _change_state_to_chase() -> void:
-	if parent.global_position.distance_to(player.global_position) < parent.stats.attack_range:
-		emit_signal('change_state_to', self,'chase')
-		print('asdas')
+func _change_state_to() -> void:
+	# chase
+	if parent.global_position.distance_to(player.global_position) < parent.stats.chase_range:
+		emit_signal('change_state_to', self, 'chase')
