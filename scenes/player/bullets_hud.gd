91546432181beeb0a23bb2c8bf754e0cd40542ab
c-dez <c-dez@proton.me>
2026-01-17 15:14:11 -0600

    implementado reload

--- scenes/player/bullets_hud.gd
+++ scenes/player/bullets_hud.gd
@@ -1,7 +1,7 @@
 extends TextureProgressBar
 
 
-# BulletsHUD
+# HUD para indicar el numero de balas que se pueden disparar
 
 @onready var proyectile_component: ProyectileComponent = get_parent().get_parent().get_node('ProyectileComponent')
 
@@ -12,10 +12,10 @@ func _ready():
 	var max_percentage := 100
 	value = max_percentage
 	max_bullets = proyectile_component.max_bullets
-	proyectile_component.connect('player_shoot', on_player_shoot)
+	proyectile_component.connect('current_bullets_update', on_current_bullets_update)
 
 	
-func on_player_shoot(_current_bullets: int) -> void:
+func on_current_bullets_update(_current_bullets: int) -> void:
 	current_bullets = _current_bullets
 	var percent := (current_bullets / max_bullets) * 100
 	value = percent
--- scenes/player/proyectile_component.gd
+++ scenes/player/proyectile_component.gd
@@ -10,15 +10,26 @@ var current_bullets: int
 @onready var camera: Camera3D = get_parent().get_node('PlayerCamera')
 @onready var muzzle: Marker3D = camera.get_node('Muzzle')
 @onready var player: PlayerBody = get_parent()
-signal player_shoot(current_bullets: int)
+signal current_bullets_update(current_bullets: int)
+
+# reload
+var _reload_time_per_bullet: float = 0.3
+var _is_reloading: bool = false
+@onready var _reload_timer: Timer = Timer.new()
 
 
 func _ready() -> void:
+	add_child(_reload_timer)
+	_reload_timer.one_shot = true
+	_reload_timer.autostart = false
+	_reload_timer.connect('timeout', on_reload_timer_timeout)
+
 	current_bullets = max_bullets
 
 
 func _process(_delta: float) -> void:
 	_set_position()
+	reload(_delta)
 
 
 func _physics_process(_delta: float) -> void:
@@ -28,17 +39,35 @@ func _physics_process(_delta: float) -> void:
 func shoot() -> void:
 	if ray.is_colliding():
 		if Input.is_action_just_pressed('left_mb') and current_bullets > 0:
-			current_bullets -= 1
-			emit_signal('player_shoot', current_bullets)
-			var target := ray.get_collision_point()
+			if _is_reloading == false:
+				current_bullets -= 1
+				emit_signal('current_bullets_update', current_bullets)
+				var target := ray.get_collision_point()
 
-			var b := bullet.instantiate()
-			b.damage = player.stats.damage
-			b.bullet_radius = 0.1
-			b.type = b.PLAYER
-			b.damage = player.stats.damage
-			muzzle.add_child(b)
-			b.look_at(target, Vector3.UP)
+				var b := bullet.instantiate()
+				b.damage = player.stats.damage
+				b.bullet_radius = 0.1
+				b.type = b.PLAYER
+				b.damage = player.stats.damage
+				muzzle.add_child(b)
+				b.look_at(target, Vector3.UP)
+
+
+func reload(_delta) -> void:
+	if current_bullets == 0:
+		if Input.is_action_just_pressed('r_key') or Input.is_action_just_pressed('left_mb'):
+			print('reload')
+			_is_reloading = true
+			_reload_timer.start(_reload_time_per_bullet)
+
+
+func on_reload_timer_timeout() -> void:
+	current_bullets += 1
+	emit_signal('current_bullets_update', current_bullets)
+	if current_bullets < max_bullets:
+		_reload_timer.start(_reload_time_per_bullet)
+	elif current_bullets == max_bullets:
+		_is_reloading = false
 
 
 func _set_position() -> void:
