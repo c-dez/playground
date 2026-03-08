22b528cf10c9b17ccc51326b2477b3fa961aeb64
c-dez <c-dez@proton.me>
2026-03-08 15:21:35 -0600

    implementado attack hitbox usando OOP

--- scenes/player/katana_player/attack.gd
+++ scenes/player/katana_player/attack.gd
@@ -1,45 +1,47 @@
 extends State
 
 var enemies_inside_attack_area_list
-var attack_area: Area3D = null
+# var attack_area: Area3D = null
 var attack_duration: float = 0.5
 @onready var sm: StateMachine = get_parent()
 @onready var timer: Timer = Timer.new()
 
+signal state_signal()
 
 func _ready():
 	add_child(timer)
 	timer.connect('timeout', on_timeout)
 
-	await sm.get_parent().ready
-	attack_area = sm.parent.attack_area
+	# await sm.get_parent().ready
+	# attack_area = sm.parent.attack_area
 
 
 func enter() -> void:
 	timer.start(attack_duration)
 	print(self )
-	attack_area.set_monitoring(true)
+	# attack_area.set_monitoring(true)
+	emit_signal('state_signal')
 
 
 func physics_process(_delta: float) -> void:
 	move(_delta)
-	attack()
+	# attack()
 	do_damage()
 
 
 func exit() -> void:
 	sm.last_state = self
 	timer.stop()
-	attack_area.set_monitoring(false)
+	# attack_area.set_monitoring(false)
 
 
-func attack() -> void:
-	if attack_area.is_monitoring():
-		if attack_area.has_overlapping_bodies():
-			enemies_inside_attack_area_list = attack_area.get_overlapping_bodies()
-			attack_area.set_monitoring(false)
-		else:
-			enemies_inside_attack_area_list = null
+# func attack() -> void:
+# 	if attack_area.is_monitoring():
+# 		if attack_area.has_overlapping_bodies():
+# 			enemies_inside_attack_area_list = attack_area.get_overlapping_bodies()
+# 			attack_area.set_monitoring(false)
+# 		else:
+# 			enemies_inside_attack_area_list = null
 
 
 func do_damage() -> void:
---
+++ scripts/hitbox.gd
@@ -0,0 +1,53 @@
+extends Area3D
+class_name Hitbox
+
+@export var state: State
+@export var parent: CharacterBody3D
+@export var hitbox_duration_time: float = 0.3
+
+@onready var duration_timer := Timer.new()
+var enemies_inside = null
+
+func _ready() -> void:
+	_set_duration_timer_propeties()
+	state.connect('state_signal', on_state_signal)
+	
+
+func _physics_process(_delta: float) -> void:
+	if duration_timer.time_left as bool:
+		attack()
+		do_damage()
+	pass
+
+
+func attack() -> void:
+	if is_monitoring():
+		if has_overlapping_bodies():
+			enemies_inside = get_overlapping_bodies()
+			set_monitoring(false)
+		else:
+			enemies_inside = null
+
+
+func do_damage() -> void:
+	if enemies_inside != null:
+		for enemy in enemies_inside:
+			if enemy.has_method('take_damage'):
+				enemy.take_damage(parent.stats.damage)
+		enemies_inside = null
+
+
+func on_timer_timeout() -> void:
+	set_monitoring(false)
+
+
+func on_state_signal() -> void:
+	duration_timer.start(hitbox_duration_time)
+	set_monitoring(true)
+
+
+func _set_duration_timer_propeties() -> void:
+	duration_timer.one_shot = true
+	duration_timer.autostart = false
+	add_child(duration_timer)
+	duration_timer.connect('timeout', on_timer_timeout)
