5a9a7efb341bc9243e48ed87e40cf8c61e5bd4d7
c-dez <c-dez@proton.me>
2026-03-08 15:32:27 -0600

    cambiado comportamiento de ground pound hitbox a OOP

--- scenes/player/katana_player/ground_area.gd
+++ scenes/player/katana_player/ground_area.gd
@@ -1,53 +1,11 @@
-extends Area3D
-
-
-# area de dano al hacer ground pound
-
-var enemies_inside_attack_area_list
-var area_damage_time: float = 0.3
-@export var ground_pound: State
-@onready var parent: CharacterBody3D = get_parent()
-@onready var timer := Timer.new()
+extends Hitbox
 
 
 func _ready() -> void:
-	timer.one_shot = true
-	timer.autostart = false
-	add_child(timer)
-	top_level = true
-	timer.connect('timeout', on_timer_timeout)
-	ground_pound.connect('ground_pound_area_signal', on_ground_pound_signal)
+    super._ready()
+    top_level = true
 
 
-func _physics_process(_delta: float) -> void:
-	if timer.time_left as bool:
-		attack()
-		do_damage()
-
-
-func attack() -> void:
-	if is_monitoring():
-		if has_overlapping_bodies():
-			enemies_inside_attack_area_list = get_overlapping_bodies()
-			set_monitoring(false)
-		else:
-			enemies_inside_attack_area_list = null
-
-
-func do_damage() -> void:
-	if enemies_inside_attack_area_list != null:
-		for item in enemies_inside_attack_area_list:
-			if item.has_method('take_damage'):
-				item.take_damage(parent.stats.damage)
-		enemies_inside_attack_area_list = null
-
-
-func on_ground_pound_signal() -> void:
-	global_position = parent.global_position
-	timer.start(area_damage_time)
-	set_monitoring(true)
-	pass
-
-
-func on_timer_timeout() -> void:
-	set_monitoring(false)
+func on_state_signal() -> void:
+    super.on_state_signal()
+    global_position = parent.global_position
--- scenes/player/katana_player/ground_pound.gd
+++ scenes/player/katana_player/ground_pound.gd
@@ -6,7 +6,7 @@ extends State
 var drop_speed: int = 25
 @onready var sm: StateMachine = get_parent()
 
-signal ground_pound_area_signal()
+signal state_signal()
 
 
 func enter() -> void:
@@ -30,4 +30,4 @@ func _change_state_to() -> void:
     if sm.parent.is_on_floor():
         emit_signal('change_state_to', self , 'groundpoundonfloor')
         # signal de dano en area en el suelo
-        emit_signal('ground_pound_area_signal')
+        emit_signal('state_signal')
