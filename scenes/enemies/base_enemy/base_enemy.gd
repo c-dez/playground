d9cd42d63e695b5be11bd8560cb1d743c1d797d7
c-dez <c-dez@proton.me>
2026-03-12 19:06:33 -0600

    gravity()

--- scenes/enemies/base_enemy/base_enemy.gd
+++ scenes/enemies/base_enemy/base_enemy.gd
@@ -6,13 +6,17 @@ class_name BaseEnemy
 var current_health: int:
     set(value):
         current_health = clamp(value, 0, max_health)
-   
+
+@onready var sm:StateMachine = get_node('StateMachine')
 
 func _init() -> void:
     current_health = max_health
 
-@onready var sm:StateMachine = get_node('StateMachine')
 
+
+func gravity() -> void:
+    if not is_on_floor():
+        velocity.y = -10
 
 
 func take_damage(damage:int) -> void:
