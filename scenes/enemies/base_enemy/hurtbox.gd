91da559424b21389d5552bd1caa1daa33c06621c
c-dez <c-dez@proton.me>
2026-03-13 16:03:40 -0600

    separado logica de cuando recibe dano, si en aire o en suelo

--- scenes/enemies/base_enemy/hurtbox.gd
+++ scenes/enemies/base_enemy/hurtbox.gd
@@ -12,7 +12,10 @@ func _on_area_entered(hitbox: Hitbox) -> void:
         if owner.has_method('launch_to_air'):
             if owner.is_on_floor():
                 owner.launch_to_air()
-                owner.take_damage(attack_damage)
+                owner.take_damage(ground_pound_damage)
 
     if hitbox.name == 'AttackHitbox':
-        owner.juggle()
+        if owner.is_on_floor():
+            owner.take_damage(attack_damage)
+        else:
+            owner.juggle()
