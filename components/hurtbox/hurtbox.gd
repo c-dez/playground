672ef422815b353410174e8720fec2a45bb1c386
c-dez <c-dez@proton.me>
2026-03-12 18:35:08 -0600

    take_damage()

--- components/hurtbox/hurtbox.gd
+++ components/hurtbox/hurtbox.gd
@@ -1,19 +1,18 @@
 extends Area3D
 class_name Hurtbox
 
-## Nombre de el metodo a usar cuando area_entered
-var method_callable_string:String = ''
 
 
 func _ready() -> void:
-    connect('area_entered', _on_area_entered)
-    # if method_callable_string.is_empty():
-    #     printerr('method_callable_string empty: ', owner.name,' -> ', self)
+	connect('area_entered', _on_area_entered)
+	
 
 
 func _on_area_entered(hitbox: Hitbox) -> void:
-    if hitbox == null:
-        return
-    if owner.has_method(method_callable_string):
-        print(owner)
-    pass
+	if hitbox == null:
+		return
+	if owner.has_method('take_damage'):
+		var damage:int= hitbox.owner.stats.damage
+		owner.take_damage(damage)
+		
+	pass
---
+++ scenes/enemies/base_enemy/base_enemy.gd
@@ -0,0 +1,6 @@
+extends CharacterBody3D
+class_name BaseEnemy
+
+
+func take_damage(damage:int) -> void:
+    print(damage)
