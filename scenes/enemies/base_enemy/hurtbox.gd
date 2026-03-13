a0bcbc73fb198ee3a459620476f42167ddced67f
c-dez <c-dez@proton.me>
2026-03-13 15:58:50 -0600

    agregado variables de dano

--- scenes/enemies/base_enemy/hurtbox.gd
+++ scenes/enemies/base_enemy/hurtbox.gd
@@ -1,6 +1,9 @@
 extends Hurtbox
 
 # Base_enemy hurtbox
+var attack_damage: int = 20
+var ground_pound_damage: int = 10
+
 
 func _on_area_entered(hitbox: Hitbox) -> void:
     if hitbox == null:
@@ -8,5 +11,6 @@ func _on_area_entered(hitbox: Hitbox) -> void:
     if hitbox.name == "GroundPoundHitbox":
         if owner.has_method('launch_to_air'):
             owner.launch_to_air()
+
     if hitbox.name == 'AttackHitbox':
         owner.juggle()
