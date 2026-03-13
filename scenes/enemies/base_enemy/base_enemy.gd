26bbb8962061897ff15f227e5c26e933a942bd33
c-dez <c-dez@proton.me>
2026-03-12 19:02:34 -0600

    die()

--- scenes/enemies/base_enemy/base_enemy.gd
+++ scenes/enemies/base_enemy/base_enemy.gd
@@ -1,9 +1,26 @@
 extends CharacterBody3D
 class_name BaseEnemy
 
+
+@export var max_health : int = 100
+var current_health: int:
+    set(value):
+        current_health = clamp(value, 0, max_health)
+   
+
+func _init() -> void:
+    current_health = max_health
+
 @onready var sm:StateMachine = get_node('StateMachine')
 
 
+
 func take_damage(damage:int) -> void:
-    print(damage)
+    current_health -= damage
+    if current_health == 0:
+        die()
     pass
+
+
+func die()-> void:
+    print('die')
