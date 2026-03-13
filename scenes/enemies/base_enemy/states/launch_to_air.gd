3c2e780aea06d2adfcb3229a0d1eaba7416a0f2e
c-dez <c-dez@proton.me>
2026-03-13 15:27:27 -0600

    agregado timer

--- scenes/enemies/base_enemy/states/launch_to_air.gd
+++ scenes/enemies/base_enemy/states/launch_to_air.gd
@@ -1,13 +1,29 @@
 extends State
 
+@onready var timer := Timer.new()
+var duration_time: float = 1.0
+
+
+func _ready() -> void:
+    _set_timer_propeties()
 
 func enter() -> void:
-    print(self,' state')
-    # sm.parent.velocity.y = 200
-
+    print(self , ' state')
+    timer.start(duration_time)
 
 
 func physics_process(_delta: float) -> void:
     # sm.parent.gravity(10)
     sm.parent.velocity.y += 10 * _delta
     pass
+
+
+func _on_timer_timeout() -> void:
+    emit_signal('change_state_to', self , 'idle')
+
+
+func _set_timer_propeties() -> void:
+    add_child(timer)
+    timer.autostart = false
+    timer.one_shot = true
+    timer.connect('timeout', _on_timer_timeout)
